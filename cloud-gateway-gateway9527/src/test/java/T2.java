c943d8cb7b8e164351dd5983ff4e3fa597e36a11
jxy <jinxiaoyunupt@163.com>
2020-07-29 21:18:40 +0800

    第二次上传代码

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,14 @@
+import java.time.ZonedDateTime;
+
+/**
+ * jxy
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime jxy = ZonedDateTime.now();
+        System.out.println(jxy);
+        //2020-07-29T17:04:29.287+08:00[Asia/Shanghai]
+
+
+    }
+}
